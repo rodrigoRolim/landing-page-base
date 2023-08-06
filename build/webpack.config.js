838d413d9069b8688be4db3f9541f38b57a797fc
rodrigoRolim <rodrigorolimveras92@gmail.com>
2023-08-06 18:02:18 -0300

    more sections

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const autoprefixer = require('autoprefixer');
+
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
